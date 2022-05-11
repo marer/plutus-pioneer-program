a690add85eb24238f17900f3da06f13bb77ad16e
Marek Mizier <marek.mizier@currency-one.com>
2022-05-11 20:30:49 +0200

    week04 homework

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -1,42 +1,61 @@
-{-# LANGUAGE DataKinds          #-}
-{-# LANGUAGE DeriveAnyClass     #-}
-{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE TypeApplications   #-}
-{-# LANGUAGE TypeOperators      #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Week04.Homework where
 
-import Data.Aeson            (FromJSON, ToJSON)
-import Data.Functor          (void)
-import Data.Text             (Text)
-import GHC.Generics          (Generic)
-import Ledger
-import Ledger.Ada            as Ada
-import Ledger.Constraints    as Constraints
-import Plutus.Contract       as Contract
-import Plutus.Trace.Emulator as Emulator
+import           Data.Aeson                     ( FromJSON
+                                                , ToJSON
+                                                )
+import           Data.Functor                   ( void )
+import           Data.Text                      ( Text
+                                                , unpack
+                                                )
+import           GHC.Generics                   ( Generic )
+import           Ledger
+import           Ledger.Ada                    as Ada
+import           Ledger.Constraints            as Constraints
+import           Plutus.Contract               as Contract
+import           Plutus.Trace
+import           Plutus.Trace.Emulator         as Emulator
+import           Wallet.Emulator.Wallet         ( knownWallet
+                                                , mockWalletPaymentPubKeyHash
+                                                )
 
 data PayParams = PayParams
-    { ppRecipient :: PaymentPubKeyHash
-    , ppLovelace  :: Integer
-    } deriving (Show, Generic, FromJSON, ToJSON)
+  { ppRecipient :: PaymentPubKeyHash
+  , ppLovelace  :: Integer
+  }
+  deriving (Show, Generic, FromJSON, ToJSON)
 
 type PaySchema = Endpoint "pay" PayParams
 
 payContract :: Contract () PaySchema Text ()
 payContract = do
-    pp <- awaitPromise $ endpoint @"pay" return
-    let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    void $ submitTx tx
-    payContract
+  pp <- awaitPromise $ endpoint @"pay" return
+  let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
+  Contract.handleError (\err -> Contract.logError $ "caught: " ++ unpack err)
+    $ void
+    $ submitTx tx
+  payContract
 
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace _ _ = undefined -- IMPLEMENT ME!
+payTrace amount1 amount2 = do
+  h1 <- activateContractWallet (knownWallet 1) payContract
+  let recipient = mockWalletPaymentPubKeyHash $ knownWallet 2
+  callEndpoint @"pay" h1
+    $ PayParams { ppRecipient = recipient, ppLovelace = amount1 }
+  void $ Emulator.waitNSlots 1
+  callEndpoint @"pay" h1
+    $ PayParams { ppRecipient = recipient, ppLovelace = amount2 }
+  void $ Emulator.waitNSlots 1
 
 payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 10_000_000 20_000_000
